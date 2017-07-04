c5696eccc3d7527a24895fe218715daba05b220f
Anwesh43 <anweshthecool0@gmail.com>
2017-07-04 18:28:38 +0530

    Adding NavbarElement

--- NavbarLinkComponent.js
+++ NavbarLinkComponent.js
@@ -1,4 +1,4 @@
-const w = window.innerWidth,const h = window.innerHeight
+const w = window.innerWidth, h = window.innerHeight,fontSize = h/30
 class NavbarLinkComponent extends HTMLElement {
     constructor() {
         super()
@@ -26,5 +26,57 @@ class NavbarLinkComponent extends HTMLElement {
     }
     connectedCallback() {
         this.render()
+    }
+}
+class NavbarElement {
+    constructor(x,y,link,size) {
+        this.x = x
+        this.y = y
+        this.link = link
+        this.size = size
+        this.scale = 0
+        this.dir = 0
+    }
+    draw(context) {
+        context.fillStyle = 'white'
+        const tw = context.measureText(this.link.title).width
+        context.fillText(this.link.text,this.x+this.size/2-tw/2,this.y)
+        context.strokeStyle = '#00838F'
+        const mx = this.x+this.size/2
+        for(var i=0;i<2;i++) {
+            context.beginPath()
+            context.moveTo(mx,this.y+fontSize/2)
+            context.lineTo(mx-(2*i-1)*(this.size/2)*this.scale,this.y+fontSize/2)
+            context.stroke()
+        }
+    }
+    startUpdating() {
+        if(this.scale <= 0) {
+            this.dir = 1
+        }
+        if(this.scale >= 1) {
+            this.dir = -1
+        }
+    }
+    handleTap(x) {
+        const condition = x>=this.x && x<=this.x+this.size && dir == 0
+        if(condition) {
+            this.startUpdating()
+        }
+        return condition
+    }
+    update() {
+        this.scale += this.dir * 0.2
+        if(this.scale > 1) {
+            this.scale = 1
+            this.dir = 0
+        }
+        if(this.scale < 0) {
+            this.scale = 0
+            this.dir = 0
+        }
+    }
+    stopped() {
+        return this.dir == 0
     }
 }
